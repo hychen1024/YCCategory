bae3ebde4d5c114368ec99a6114c47c9ba73ccc0
胡宇琛 <huyc@mama.cn>
2017-03-23 12:19:35 +0800

    初始化项目

---
+++ YCCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCCategory
+//
+//  Created by huyuchen on 2017/3/23.
+//  Copyright © 2017年 Hyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCCategory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YCCategory
+//
+//  Created by huyuchen on 2017/3/23.
+//  Copyright © 2017年 Hyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
